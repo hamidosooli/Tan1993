0d8022e8ee438dc953e633b63f9a37edafb059a1
hamidosooli <hamid.osooli@gmail.com>
2022-05-17 19:30:40 -0400

    final version

--- case1_No_Scout.py
+++ case1_No_Scout.py
@@ -1,22 +1,19 @@
 import numpy as np
 import h5py
 
-NUM_EPISODES = 500
-Hunter_VFD = 2  # Hunter's visual field depth
-Scout_VFD = 2  # Scout's visual field depth
-max_step = 1000
+
 # Actions
 FORWARD = 0
 BACKWARD = 1
 RIGHT = 2
 LEFT = 3
-NORTHEAST = 4
-NORTHWEST = 5
-SOUTHEAST = 6
-SOUTHWEST = 7
-ACTIONS = [FORWARD, BACKWARD, RIGHT, LEFT]  # , NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST]
+ACTIONS = [FORWARD, BACKWARD, RIGHT, LEFT]
 nA = len(ACTIONS)
+
+NUM_EPISODES = 500
+Hunter_VFD = 2  # Hunter's visual field depth
 gamma = .9
+
 Row_num = 10
 Col_num = 10
 row_lim = 9
@@ -41,28 +38,17 @@ def movement(position, action):
         next_position = [row, min(column + 1, column_lim)]
     elif action == 3:  # left
         next_position = [row, max(column - 1, 0)]
-    # elif action == 4:  # northeast
-    #     next_position = [max(row - 1, 0), min(column + 1, column_lim)]
-    # elif action == 5:  # northwest
-    #     next_position = [max(row - 1, 0), max(column - 1, 0)]
-    # elif action == 6:  # southeast
-    #     next_position = [min(row + 1, row_lim), min(column + 1, column_lim)]
-    # elif action == 7:  # southwest
-    #     next_position = [min(row + 1, row_lim), max(column - 1, 0)]
 
     return next_position
 
 
-def transition(hunter_sensation):  #, scout_sensation, scout2hunter):
+def update_sensation(hunter_sensation):
     global default_sensation
     if abs(hunter_sensation[0]) <= Hunter_VFD and abs(hunter_sensation[1]) <= Hunter_VFD:
         row = hunter_sensation[0]
         column = hunter_sensation[1]
         default_sensation = [row, column]
-    # elif abs(scout_sensation[0]) <= Scout_VFD and abs(scout_sensation[1]) <= Scout_VFD:
-    #     row = scout2hunter[0] + scout_sensation[0]
-    #     column = scout2hunter[1] + scout_sensation[1]
-    #     default_sensation = [row, column]
+
     else:  # if there is no prey in sight, a unique default sensation is used.
         row, column = default_sensation
 
@@ -81,15 +67,15 @@ def reward(hunter_sensation_prime):
 
 def sensation2index(sensation, VFD):
     if abs(sensation[0]) <= VFD and abs(sensation[1]) <= VFD:
-        index = (sensation[0]+VFD) * (2*VFD+1) + (sensation[1]+VFD)
+        index = (sensation[0] + VFD) * (2 * VFD + 1) + (sensation[1] + VFD)
     else:
-        index = (2*Hunter_VFD+1)**2
+        index = (2 * Hunter_VFD + 1) ** 2
     return index
 
 
 def rl_agent(beta=0.8):
     global default_sensation
-    Q = np.zeros(((2*Hunter_VFD+1)**2 + 1, nA))
+    Q = np.zeros(((2 * Hunter_VFD + 1) ** 2 + 1, nA))
 
     steps = []
     rewards = []
@@ -123,31 +109,25 @@ def rl_agent(beta=0.8):
             T_scout.append(scout_pos)
             T_prey.append(prey_pos)
 
-            # scout2hunter = np.subtract(scout_pos, hunter_pos)
-            # scout_sensation = np.subtract(prey_pos, scout_pos)
             hunter_sensation_step1 = np.subtract(prey_pos, hunter_pos)
-            hunter_sensation = transition(hunter_sensation_step1)  # , scout_sensation, scout2hunter)
+            hunter_sensation = update_sensation(hunter_sensation_step1)
             idx = sensation2index(hunter_sensation, Hunter_VFD)
             hunter_probs = Boltzmann(Q[idx, :])
             hunter_action = np.random.choice(ACTIONS, p=hunter_probs)
-            # scout_action = np.random.choice(ACTIONS)
             prey_action = np.random.choice(ACTIONS)
 
             hunter_pos_prime = movement(hunter_pos, hunter_action)
-            # scout_pos_prime = movement(scout_pos, scout_action)
             prey_pos_prime = movement(prey_pos, prey_action)
 
-            # scout2hunter_prime = np.subtract(scout_pos_prime, hunter_pos_prime)
-            # scout_sensation_prime = np.subtract(prey_pos_prime, scout_pos_prime)
             hunter_sensation_prime_step1 = np.subtract(prey_pos, hunter_pos_prime)
-            hunter_sensation_prime = transition(hunter_sensation_prime_step1)  # , scout_sensation_prime, scout2hunter_prime)
+            hunter_sensation_prime = update_sensation(hunter_sensation_prime_step1)
 
             re = reward(hunter_sensation)
-            print(re)
-            A_hunter.append(hunter_action)
-            # A_scout.append(scout_action)
-            A_prey.append(prey_action)
             R.append(re)
+
+            A_hunter.append(hunter_action)
+            A_prey.append(prey_action)
+
             if default_sensation != [Hunter_VFD, Hunter_VFD]:
                 R_prime.append(re)
                 counter += 1
@@ -156,17 +136,9 @@ def rl_agent(beta=0.8):
             idx_prime = sensation2index(hunter_sensation_prime, Hunter_VFD)
             Q[idx, hunter_action] += beta * (re + gamma * np.max(Q[idx_prime, :]) - Q[idx, hunter_action])
 
-            # scout_pos = scout_pos_prime
-            # if hunter_sensation_prime != [0, 0]:
-
-            # if eps>490:
-            #     print(Q[10:20, :])if hunter_sensation_prime == [0, 0]:
             hunter_pos = hunter_pos_prime
             prey_pos = prey_pos_prime
             if hunter_sensation == [0, 0]:
-                # T_hunter.append(hunter_pos)
-                # T_scout.append(scout_pos)
-                # T_prey.append(prey_pos)
                 steps.append(t_step)
                 see_steps.append(s)
                 rewards.append(sum(R))
@@ -179,9 +151,7 @@ def rl_agent(beta=0.8):
 
 T_hunter, T_scout, T_prey, A_hunter, A_scout, A_prey, rewards, steps, see_rewards, see_steps, Q = rl_agent(beta=0.8)
 
-
 with h5py.File(f'Tan1993_case1.hdf5', "w") as f:
-
     f.create_dataset('T_hunter', data=T_hunter)
     f.create_dataset('T_scout', data=T_scout)
     f.create_dataset('T_prey', data=T_prey)
