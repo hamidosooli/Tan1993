1f7e62c7de9e3406370562aae48428e3be7ec49e
hamidosooli <hamid.osooli@gmail.com>
2022-05-20 11:48:15 -0400

    more experiments

--- plotter.py
+++ plotter.py
@@ -5,9 +5,10 @@ import h5py
 
 hunter_vfd = 2
 scout_vfd = 2
-
-# exp = 'No Scout'
-exp = 'With Random Scout'
+# mode = 'Multi Runs'
+mode = 'Single Run'
+exp = 'No Scout'
+# exp = 'With Random Scout'
 # exp = 'With Leaning Scout'
 
 if exp == 'No Scout':
@@ -29,7 +30,11 @@ rewards = []
 steps = []
 see_rewards = []
 see_steps = []
-
+if mode == 'Multi Runs' and exp == 'With Leaning Scout':
+    rewards_scout = []
+    see_rewards_scout = []
+    see_steps_scout = []
+    filename = 'Tan1993_case1_with_learning_scout_2&4_100runs.hdf5'
 Q = []
 with h5py.File(filename, 'r') as gw_ma:
     T_hunter.append(np.asarray(gw_ma['T_hunter']))
@@ -40,30 +45,56 @@ with h5py.File(filename, 'r') as gw_ma:
     steps.append(np.asarray(gw_ma['steps']))
     see_rewards.append(np.asarray(gw_ma['see_rewards']))
     see_steps.append(np.asarray(gw_ma['see_steps']))
-
     Q.append(np.asarray(gw_ma['Q']))
-
-    if exp != 'No Scout':
-        T_scout.append(np.asarray(gw_ma['T_scout']))
-        A_scout.append(np.asarray(gw_ma['A_scout']))
-        animate(T_hunter[0], T_scout[0], T_prey[0],
-                A_hunter[0], A_scout[0], A_prey[0],
-                hunter_vfd, scout_vfd, wait_time=.5, have_scout=True)
-    else:
-        animate(T_hunter[0], [], T_prey[0],
-                A_hunter[0], [], A_prey[0],
-                hunter_vfd, scout_vfd, wait_time=.5, have_scout=False)
+    if mode == 'Multi Runs' and exp == 'With Leaning Scout':
+        rewards_scout.append(np.asarray(gw_ma['rewards_scout']))
+        see_rewards_scout.append(np.asarray(gw_ma['see_rewards_scout']))
+        see_steps_scout.append(np.asarray(gw_ma['see_steps_scout']))
+    # if exp != 'No Scout':
+    #     T_scout.append(np.asarray(gw_ma['T_scout']))
+    #     A_scout.append(np.asarray(gw_ma['A_scout']))
+    #     animate(T_hunter[0], T_scout[0], T_prey[0],
+    #             A_hunter[0], A_scout[0], A_prey[0],
+    #             hunter_vfd, scout_vfd, wait_time=.5, have_scout=True)
+    # else:
+    #     animate(T_hunter[0], [], T_prey[0],
+    #             A_hunter[0], [], A_prey[0],
+    #             hunter_vfd, scout_vfd, wait_time=.5, have_scout=False)
 
 plt.figure('Rewards')
 plt.xlabel('Episodes')
 plt.ylabel('Sum of Rewards during each Episode')
-plt.plot(rewards[0])
-plt.plot(see_rewards[0])
+if mode == 'Multi Runs':
+    plt.plot(np.mean(np.asarray(rewards[0]), axis=0))
+    plt.plot(np.mean(np.asarray(see_rewards[0]), axis=0))
+else:
+    plt.plot(rewards[0])
+    plt.plot(see_rewards[0])
 plt.legend(['Total rewards for hunter', 'When we have data'])
+
 plt.figure('Steps')
 plt.xlabel('Episodes')
 plt.ylabel('Number of Steps on each Episode')
-plt.plot(steps[0])
-plt.plot(see_steps[0])
+if mode == 'Multi Runs':
+    plt.plot(np.mean(np.asarray(steps[0]), axis=0))
+    plt.plot(np.mean(np.asarray(see_steps[0]), axis=0))
+else:
+    plt.plot(steps[0])
+    plt.plot(see_steps[0])
 plt.legend(['Total steps', 'When we have data'])
+
+if mode == 'Multi Runs' and exp == 'With Leaning Scout':
+    plt.figure('Rewards_scout')
+    plt.xlabel('Episodes')
+    plt.ylabel('Sum of Rewards during each Episode')
+    plt.plot(np.mean(np.asarray(rewards_scout[0]), axis=0))
+    plt.plot(np.mean(np.asarray(see_rewards_scout[0]), axis=0))
+    plt.legend(['Total rewards for hunter', 'When we have data'])
+
+    plt.figure('Steps_scout')
+    plt.xlabel('Episodes')
+    plt.ylabel('Number of Steps on each Episode')
+    plt.plot(np.mean(np.asarray(see_steps_scout[0]), axis=0))
+    plt.legend(['Total steps', 'When we have data'])
+
 plt.show()
